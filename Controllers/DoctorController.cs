8e6b05ebdf357205883e4fdcb0c3ed237658ab0e
Nour Zakarya <noorzakria25@gmail.com>
2024-03-26 03:26:27 +0200

    first commit

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class DoctorController : Controller
+    {
+        SampleDataDoctor sample = new SampleDataDoctor();
+        public IActionResult Index()
+        {
+            return View(sample.doctors.ToList());
+        }
+        public IActionResult BookAnAppointment(int id)
+        {
+
+            return View(sample.doctors.First(e=>e.Id==id));
+        }
+        
+    }
+}
